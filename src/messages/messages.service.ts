1afd65583f7ed96839528660a4a768b4cd5e1576
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-10-23 16:24:37 -0300

    feat: added delete end point

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -24,5 +24,18 @@ export class MessagesService {
 
   create(message: Message) {
     this.messages.push(message);
+    return message;
+  }
+
+  update(id: number, message: Message) {
+    const index = this.messages.findIndex((message) => message.id === id);
+    this.messages[index] = message;
+    return message;
+  }
+
+  delete(id: number) {
+    const index = this.messages.findIndex((message) => message.id === id);
+    delete this.messages[index];
+    return true;
   }
 }
