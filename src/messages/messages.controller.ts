9b56094f41b1e85d0f93ae3797ba7272e0b1adc7
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-10-23 16:15:15 -0300

    feat: added create end point

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Message } from './Message';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
@@ -8,5 +9,15 @@ export class MessagesController {
   @Get()
   findAll() {
     return this.messagesService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param() params) {
+    return this.messagesService.findById(+params.id);
+  }
+
+  @Post()
+  create(@Body() message: Message) {
+    return this.messagesService.create(message);
   }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Message } from './Message';
 
 @Injectable()
 export class MessagesService {
-  private messages = [
+  private messages: Message[] = [
     {
       id: 1,
       text: 'a',
@@ -13,7 +14,15 @@ export class MessagesService {
     },
   ];
 
+  findById(id: number) {
+    return this.messages.find((message) => message.id === id);
+  }
+
   findAll() {
     return this.messages;
+  }
+
+  create(message: Message) {
+    this.messages.push(message);
   }
 }
